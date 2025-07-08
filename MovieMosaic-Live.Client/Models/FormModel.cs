97e1eb328138ffea0b6f19425378d2205a6c3a10
Mike Williams <mikey32905@gmail.com>
2025-07-08 17:07:45 -0400

    Started to add game controls .

---
+++ MovieMosaic-Live.Client/Models/FormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieMosaic_Live.Client.Models
+{
+    public class FormModel
+    {
+        [Required]
+        public string? MovieTitle { get; set; }
+        public string? ReleaseDate { get; set; }
+    }
+}
