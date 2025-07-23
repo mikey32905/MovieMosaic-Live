2c78938e3af0e4d086bb3d255932831e23e5407c
Mike Williams <mikey32905@gmail.com>
2025-07-23 16:50:49 -0400

    wired up guess button.
    addes correct/incorrect guesses to guess tab list.
    
    reveals all squares if correct guess sent.

---
+++ MovieMosaic-Live.Client/Models/Guess.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieMosaic_Live.Client.Models
+{
+    public class Guess
+    {
+        public string? Title { get; set; } = "";
+        public string? ReleaseYear { get; set; } = "";
+        public bool Correct { get; set; } = false;
+
+    }
+}
