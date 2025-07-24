83982652fe7a3c5916c337ebed6e23d0c3601647
Mike Williams <mikey32905@gmail.com>
2025-07-24 16:50:31 -0400

    scaffolded in poster with overlay, and guess entering textbox and button for ShowMosaic.

---
+++ MovieMosaic-Live.Client/Models/ShowFormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieMosaic_Live.Client.Models
+{
+    public class ShowFormModel
+    {
+        [Required]
+        public string? ShowTitle { get; set; }
+        public string? FirstAiredDate { get; set; }
+    }
+}
